1a360899a8b037e8c70d3519f2f9ba102728574e
bakkarxp@yahoo.com <Marwa_112015>
2020-09-17 14:44:06 +0300

    Median problem solved.

--- Level2/week_01/Median.cpp
+++ Level2/week_01/Median.cpp
@@ -4,16 +4,67 @@
 
 
 #include <iostream>
+#include <math.h>
 using std::cin;
 using std::cout;
 using std::endl;
 
+void swap(int *a,int *b){
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
 
-int main() {
-    int x;
-    while (cin>>x){
-        cout<<x<<endl;
+void bubbleSort(int numbers[],int n){
+    int i, j;
+    for(i=0; i<n-1; i++){
+        for(j=0; j<n-i-1; j++){
+            if(numbers[j] > numbers[j+1])
+                swap(&numbers[j],&numbers[j+1]);
+        }
     }
-    std::cout << "Hello, World!" << std::endl;
+}
+
+bool isOdd(int n){
+    int reminder;
+    reminder = n%2;
+    if(reminder == 0)
+    {
+        return false;
+    } else{
+        return true;
+    }
+
+}
+
+int median(int numbers[], int n){
+    int medianIndex;
+    if(isOdd(n)){
+        medianIndex =(n-1)/2;
+        return numbers[medianIndex];
+    }else{
+        medianIndex = n/2;
+        return std::ceil((numbers[medianIndex]+numbers[medianIndex-1])/2);
+    }
+
+}
+
+int numbers[10000];
+int medians[10000];
+int main() {
+
+
+
+    // Get median
+    int x{0};
+    // Get Input
+    while (cin>>numbers[x]){
+        // Sort array
+        bubbleSort(numbers,x+1);
+        cout<< median(numbers,x+1)<<endl;
+        x++;
+    }
+
+
     return 0;
 }
