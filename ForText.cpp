8def8c9b453053cd4aeb0c0169254fa1e53bc3ab
Bakkarxp <bakkarxp@yahoo.com>
2020-10-01 19:09:27 +0300

    Playing around BigInt class

--- ForText.cpp
+++ ForText.cpp
@@ -6,12 +6,44 @@
 
 using std::vector;
 using std::cout;
+using std::endl;
+
+void equalizeSize(vector<char> &num1,vector<char> &num2){
+
+    int sizeDiff;
+    if(num1.size()>num2.size()){
+        sizeDiff = num1.size()-num2.size();
+    } else{
+        sizeDiff = num2.size()-num1.size();
+    }
+
+    // equalize vector sizes
+    if(num1.size()>num2.size()){
+        num2.insert(num2.begin(),sizeDiff,'0');
+    } else{
+        num1.insert(num1.begin(),sizeDiff,'0');
+    }
+
+    for(char c:num1){
+        cout<<c;
+    }
+    cout<<endl;
+    for(char c:num2){
+        cout<<c;
+    }
+    cout<<endl;
+}
+
 int main()
 {
-    vector<char> num1={'8','8','2','6'};
-    vector<char> num2={'9','6','8','9'};
+
+    vector<char> num1={'8','8','3','6','2','0','9'};
+    vector<char> num2={'9','5','8','9'};
+
+    equalizeSize(num1,num2);
+
     char carry{'0'};
-    vector<char> sum{'0','0','0','0'};
+    vector<char> sum(num1.size(),'0');
     for(int i=num1.size()-1; i>=0;i--){
         sum[i]=((carry-'0')+(num1[i]-'0')+(num2[i]-'0'))+'0';
         if(i!=0){
